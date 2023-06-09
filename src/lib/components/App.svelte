74f99a2047e9a2619bf94e5dc0ae8cd229bb3049
ImHamba <callmehamba@gmail.com>
2023-06-09 18:49:46 +1000

    added basic site layout + modal

---
+++ src/lib/components/App.svelte
@@ -0,0 +1,25 @@
+<script>
+    import SnapScroll from "./SnapScrollPanel.svelte";
+    import ContactModal from "./ContactModal.svelte";
+
+    let showContactModal = false;
+
+    $: console.log(showContactModal);
+
+    const toggleContactModal = () => {
+        showContactModal = !showContactModal;
+    };
+</script>
+
+<main>
+    <ContactModal visible={showContactModal} on:click={toggleContactModal}/>
+    <button on:click={toggleContactModal} />
+    <SnapScroll numPages={8} />
+</main>
+
+<style>
+    main {
+        width: 100%;
+        height: 100%;
+    }
+</style>
---
+++ src/lib/components/Modal.svelte
@@ -0,0 +1,38 @@
+<script>
+    import { fade } from 'svelte/transition'
+
+    export let visible = false;
+    
+</script>
+
+{#if visible}
+    <div transition:fade class="backdrop" on:click|self on:keypress|self>
+        <div class="modal">
+            <button on:click>x</button>
+            <slot/>
+        </div>
+    </div>
+{/if}
+
+<style>
+    .backdrop {
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        background: rgba(0, 0, 0, 0.8);
+    }
+
+    .modal {
+        padding: 10px;
+        border-radius: 10px;
+        width: 50%;
+        text-align: center;
+        background: white;
+
+        position: absolute;
+        float: left;
+        left: 50%;
+        top: 40%;
+        transform: translate(-50%, -50%);
+    }
+</style>
